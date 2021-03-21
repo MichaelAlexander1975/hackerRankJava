ac8c1cd0223f7b31883520db292ed5a7fe4238a6
MichaelCaluris <45521340+MichaelCaluris@users.noreply.github.com>
2021-03-21 03:35:29 -0500

    Create chessBoard.java

---
+++ chessBoard.java
@@ -0,0 +1,12 @@
+
+static String solve(List<List<Integer>> b) 
+{
+    int n = b.size ();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 1; j < n; ++j) {
+            if (b.get (i).get (j) == b.get (i).get (j - 1)) return "No";
+            if (b.get (j).get (i) == b.get (j - 1).get (i)) return "No";
+        }
+    }
+    return "Yes";
+}
